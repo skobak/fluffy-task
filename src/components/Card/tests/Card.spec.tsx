ca5503e1fd9c0eff68369e0aac7f934bc9316154
Alexander Skobeltsyn <skob@Alex-Mac.local>
2023-06-05 12:12:25 +0200

    fix(storybook,test): fix build with new Cat model, adjust pre-push
    - Add new model everywhere
    - Add build on pre-push to avoid failiure on github Actions

--- src/components/Card/tests/Card.spec.tsx
+++ src/components/Card/tests/Card.spec.tsx
@@ -1,18 +1,27 @@
 import { test, expect } from '@playwright/experimental-ct-react'
+import { Cat } from '../../../models/Cat'
 import Card from '../../Card'
 
 test.use({ viewport: { width: 500, height: 500 } })
 
 test('should work', async ({ mount }) => {
-  const component = await mount(
-    <Card title="Super cat" description="here is a cat description" />
-  )
+  const cat: Cat = {
+    id: '1',
+    name: 'Super cat',
+    bio: 'here is a cat description'
+  }
+  const component = await mount(<Card cat={cat} />)
   await expect(component).toContainText('Super cat')
   await expect(component).toContainText('here is a cat description')
 })
 
 test('should show no description provided ', async ({ mount }) => {
-  const component = await mount(<Card title="Super cat" description="" />)
+  const cat: Cat = {
+    id: '1',
+    name: 'Super cat'
+  }
+
+  const component = await mount(<Card cat={cat} />)
   await expect(component).toContainText('Super cat')
   await expect(component).toContainText('No description provided')
 })
