57456a924d3eaced1577467426208e4cce7e08c8
Alexander Skobeltsyn <skob@Alex-Mac.local>
2023-06-05 11:36:54 +0200

    feat: add dialogConfirmation when user delete the cat

---
+++ src/components/DialogConfirmation/DialogConfirmation.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+// Define the type for the props
+interface DialogConfirmationProps {
+  cancel: () => void
+  confirm: () => void
+  message: string
+  imageURL?: string
+}
+
+// Create the component
+const DialogConfirmation: React.FC<DialogConfirmationProps> = ({
+  cancel,
+  confirm,
+  message,
+  imageURL
+}) => {
+  return (
+    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-50">
+      <div className="w-1/3 rounded-lg bg-white">
+        <div>{imageURL && <img src={imageURL} alt="cat" />}</div>
+        <div className="p-6">
+          <p className="mb-4 text-lg">{message}</p>
+          <div className="flex justify-end">
+            <button
+              className="mr-2 text-gray-700 hover:text-red-600"
+              onClick={cancel}
+            >
+              Cancel
+            </button>
+            <button
+              className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+              onClick={confirm}
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DialogConfirmation
--- src/pages/CatListPage.tsx
+++ src/pages/CatListPage.tsx
@@ -1,34 +1,60 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import NewCard from '../components/NewCard'
 import { Cat, CatContext } from '../provider/CatProvider'
 import { v4 as uuidv4 } from 'uuid'
 import Card from '../components/Card'
+import DialogConfirmation from '../components/DialogConfirmation'
+const newCatBlank: Cat = {
+  id: uuidv4(),
+  name: 'The Cat',
+  birthdayDate: '2021-01-01',
+  bio: '',
+  photo: '',
+  gender: 'female'
+}
 
 const CatListPage: React.FC = () => {
   const { cats, addCat, deleteCat, updateCat } = useContext(CatContext)
 
-  const newCatBlank: Cat = {
-    id: uuidv4(),
-    name: 'The Cat',
-    birthdayDate: '2021-01-01',
-    bio: '',
-    photo: '',
-    gender: 'female'
-  }
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+  const [currentCat, setCurrentCat] = useState<Cat | null>(null)
 
   return (
-    <div className="flex w-full flex-wrap gap-4">
-      {cats.map((cat: Cat) => (
-        <div key={cat.id}>
-          <Card title={cat.name} description={cat.bio} />
-          <button onClick={() => deleteCat(cat.id)}>Delete</button>
-          <button onClick={() => updateCat({ ...cat, name: 'The Cat2' })}>
-            Update
-          </button>
+    <div>
+      {isConfirmationOpen && (
+        <dialog open>
+          <DialogConfirmation
+            cancel={() => setIsConfirmationOpen(false)}
+            confirm={() => {
+              setIsConfirmationOpen(false)
+              deleteCat(currentCat?.id || '')
+            }}
+            message={`Are you sure you want to delete ${currentCat?.name} ?`}
+            imageURL={currentCat?.photo}
+          />
+        </dialog>
+      )}
+      <div className="flex w-full flex-wrap gap-4">
+        {cats.map((cat: Cat) => (
+          <div key={cat.id}>
+            <Card title={cat.name} description={cat.bio} />
+            <button
+              onClick={() => {
+                setIsConfirmationOpen(true)
+                setCurrentCat(cat)
+              }}
+            >
+              Delete
+            </button>
+            <button onClick={() => updateCat({ ...cat, name: 'The Cat2' })}>
+              Update
+            </button>
+          </div>
+        ))}
+        <div>
+          <NewCard onClick={() => addCat(newCatBlank)} />
         </div>
-      ))}
-      <div>
-        <NewCard onClick={() => addCat(newCatBlank)} />
       </div>
     </div>
   )
