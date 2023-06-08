1a16a7b379722c6537330116d15a247ae9bf2cc5
Alexander Skobeltsyn <skob@Alex-Mac.local>
2023-06-08 09:07:42 +0200

    refactor: cleanup

--- src/pages/CatListPage/components/CardFormModal.tsx
+++ src/pages/CatListPage/components/CardFormModal.tsx
@@ -1,6 +1,9 @@
+import React from 'react'
 import CardForm from '../../../components/CardForm'
 import { Cat } from '../../../models/Cat'
 import { v4 as uuidv4 } from 'uuid'
+import { CardFormData } from '../../../components/CardForm/CardFormData'
+
 interface CardFormModalProps {
   currentCat: Cat | null
   onClose: () => void
@@ -8,36 +11,39 @@ interface CardFormModalProps {
   updateCat: (cat: Cat) => void
 }
 
+const modalStyle =
+  'fixed left-0 top-0 z-50 h-screen w-screen items-center justify-center overflow-auto md:flex md:bg-black md:bg-opacity-50'
+
 const CardFormModal: React.FC<CardFormModalProps> = ({
   currentCat,
   onClose,
   addCat,
   updateCat
-}) => (
-  <dialog open>
-    <div className="fixed left-0 top-0 z-50 h-screen w-screen items-center justify-center overflow-auto md:flex md:bg-black md:bg-opacity-50">
-      <CardForm
-        cat={currentCat}
-        cancel={onClose}
-        confirm={async (data, fileData) => {
-          if (currentCat == null) {
-            addCat({
-              id: uuidv4(),
-              ...data,
-              photo: fileData || ''
-            })
-          } else {
-            updateCat({
-              id: currentCat.id || '',
-              ...data,
-              photo: fileData !== null ? fileData : currentCat.photo || ''
-            })
-          }
-          onClose()
-        }}
-      />
-    </div>
-  </dialog>
-)
+}) => {
+  const handleConfirm = async (data: CardFormData, fileData: string | null) => {
+    if (currentCat == null) {
+      addCat({
+        id: uuidv4(),
+        ...data,
+        photo: fileData || ''
+      })
+    } else {
+      updateCat({
+        id: currentCat.id || '',
+        ...data,
+        photo: fileData !== null ? fileData : currentCat.photo || ''
+      })
+    }
+    onClose()
+  }
+
+  return (
+    <dialog open>
+      <div className={modalStyle}>
+        <CardForm cat={currentCat} cancel={onClose} confirm={handleConfirm} />
+      </div>
+    </dialog>
+  )
+}
 
 export default CardFormModal
--- src/pages/CatListPage/components/ConfirmationModal.tsx
+++ src/pages/CatListPage/components/ConfirmationModal.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import DialogConfirmation from '../../../components/DialogConfirmation'
 import { Cat } from '../../../models/Cat'
 
@@ -11,18 +12,24 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   currentCat,
   onClose,
   deleteCat
-}) => (
-  <dialog open>
-    <DialogConfirmation
-      cancel={onClose}
-      confirm={() => {
-        onClose()
-        deleteCat(currentCat?.id || '')
-      }}
-      message={`Are you sure you want to delete ${currentCat?.name} ?`}
-      imageURL={currentCat?.photo}
-    />
-  </dialog>
-)
+}) => {
+  const handleConfirm = () => {
+    onClose()
+    deleteCat(currentCat?.id || '')
+  }
+
+  const message = `Are you sure you want to delete ${currentCat?.name} ?`
+
+  return (
+    <dialog open>
+      <DialogConfirmation
+        cancel={onClose}
+        confirm={handleConfirm}
+        message={message}
+        imageURL={currentCat?.photo}
+      />
+    </dialog>
+  )
+}
 
 export default ConfirmationModal
